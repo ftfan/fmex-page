53b216100635843bf96b02d73e9a5f124491e5ed
imconfig <982748666@qq.com>
2020-07-12 17:41:10 +0800

    loading

---
+++ src/lib/page-loading.ts
@@ -0,0 +1,29 @@
+class LoadingClose {}
+
+const el = document.getElementById('page-loader-model')!;
+const data: LoadingClose[] = [];
+
+export const PageLoading = () => {
+  const close = new LoadingClose();
+  DataPush(close);
+
+  return () => DataPop(close);
+};
+
+function DataPush(close: LoadingClose) {
+  data.push(close);
+  CheckData();
+}
+
+function DataPop(close: LoadingClose) {
+  data.splice(data.indexOf(close), 1);
+  CheckData();
+}
+
+function CheckData() {
+  if (data.length === 0) {
+    el.className = '';
+  } else {
+    el.className = 'hide';
+  }
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,26 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Index from '@/views/Index.vue';
+import { PageLoading } from '@/lib/page-loading';
 
 Vue.use(VueRouter);
 
+function AsyncLoader(resolve: any, reject: any, component: Promise<any>) {
+  const close = PageLoading();
+  component
+    .then(resolve)
+    .catch(() => {
+      alert('资源加载失败，请稍后再试');
+      reject('net error');
+    })
+    .finally(close);
+}
+
 const routes: Array<RouteConfig> = [
   { path: '/', name: 'Index', component: Index },
-  { path: '/About', name: 'About', component: () => import('@/views/About.vue') },
-  { path: '/Analysis', name: 'Analysis', component: () => import('@/views/Analysis.vue') },
-  { path: '/Friend', name: 'Friend', component: () => import('@/views/Friend.vue') },
+  { path: '/About', name: 'About', component: (s, j) => AsyncLoader(s, j, import('@/views/About.vue')) },
+  { path: '/Analysis', name: 'Analysis', component: (s, j) => AsyncLoader(s, j, import('@/views/Analysis.vue')) },
+  { path: '/Friend', name: 'Friend', component: (s, j) => AsyncLoader(s, j, import('@/views/Friend.vue')) },
 ];
 
 const router = new VueRouter({
