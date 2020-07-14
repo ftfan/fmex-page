8e5bc5404e6261bc1154309d13a12738c7d276ba
imconfig <982748666@qq.com>
2020-07-14 12:05:10 +0800

    --新增描述

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="main about" transition="normal">
     <h1>关于这个网站</h1>
-    <p>GitHub: <a href="https://github.com/ftfan/fmex-page" target="_blank">https://github.com/ftfan/fmex-page</a></p>
-    <p>联系方式： imconfig@aliyun.com</p>
+    <p>本网站代码开源，使用 GitHub Pages 部署</p>
+    <p>欢迎提pr、issues</p>
+    <p>GitHub 源码: <a href="https://github.com/ftfan/fmex-page" target="_blank">https://github.com/ftfan/fmex-page</a></p>
+    <p>有其他数据视图想法，联系方式： imconfig@aliyun.com</p>
   </div>
 </template>
 
